d277d24bf3f08781ef9c3391f7ae1ad1c7ad879f
EduardoSig2210 <00012523@uca.edu.sv>
2026-03-20 23:29:22 -0600

    Feat: terminando ejercicio

--- Ejercicio_2/app/src/main/java/com/example/ejercicio2/Calculadora.kt
+++ Ejercicio_2/app/src/main/java/com/example/ejercicio2/Calculadora.kt
@@ -4,9 +4,13 @@ class Calculadora (
     val marca: String,
     val aniosDeVida: Int
 ){
+    init {
+        precio = 0
+    }
 
-
-    constructor (marca:String, aniosDeVida: Int) : this(marca, aniosDeVida)
+    constructor (marca:String, aniosDeVida: Int, precio: Int) : this(marca, aniosDeVida){
+        this.precio = precio
+    }
 
     fun suma(a: Double, b: Double): Double{
         return a + b
@@ -20,7 +24,7 @@ class Calculadora (
         return a * b
     }
 
-    fun dividir (a: Double, b: Double){
+    fun dividir (a: Double, b: Double): Double{
         if(b == 0.0){
             println("Err: No se puede dividir entre cero")
             return 0.0
