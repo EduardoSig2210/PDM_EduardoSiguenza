b41262791877520e081ecc8c8f15af3f91e591f2
EduardoSig2210 <00012523@uca.edu.sv>
2026-03-20 23:30:17 -0600

    Feat: Terminando ejercicio

--- Ejercicio_2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
+++ Ejercicio_2/app/src/main/java/com/example/ejercicio2/MainActivity.kt
@@ -1,2 +1,20 @@
 package com.example.ejercicio2
 
+import com.example.ejercicio2.Calculadora
+
+fun main(){
+    val calcu = Calculadora(
+        marca = "Casio",
+        aniosDeVida = 6
+    )
+
+    println("--- calculadora ${calcu.marca}")
+    println("Años de vida: ${calcu.aniosDeVida}")
+
+    println("15 + 5 = ${calcu.suma(15.0, 5.0)}")
+    println("15 - 5 = ${calcu.resta(15.0, 5.0)}")
+    println("15 * 5 = ${calcu.multiplicar(15.0, 5.0)}")
+    println("15 / 5 = ${calcu.dividir(15.0, 5.0)}")
+
+    println("15 / 0 = ${calcu.dividir(15.0, 0.0)}")
+}
