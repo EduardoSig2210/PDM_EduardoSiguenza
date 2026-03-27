4800e2412b78806e2ff574fe3433405eb29a0bd8
EduardoSig2210 <00012523@uca.edu.sv>
2026-03-27 16:41:22 -0600

    Iciciando Lab2

---
+++ Lab2/app/src/main/java/com/example/lab/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.example.lab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.lab.ui.theme.LabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+
+        ) {
+        val usuario: MutableState<String> = remember {
+            mutableStateOf("")}
+            TextField(
+                value = usuario.value,
+                onValueChange = {
+                    usuario.value = it
+                },
+                placeholder = {Text("Nombre")}
+                )
+
+    }
+    LazyColumn() { }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LabTheme {
+        Greeting("Android")
+    }
+}
