1118eb4dfe12580cec48aee8581afacd749769ae
EduardoSig2210 <00012523@uca.edu.sv>
2026-03-27 21:45:28 -0600

    Feat: Terminando lab 2

--- Lab2/app/src/main/java/com/example/lab/MainActivity.kt
+++ Lab2/app/src/main/java/com/example/lab/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.lab
 
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,12 +26,15 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.lab.ui.theme.LabTheme
 
 class MainActivity : ComponentActivity() {
@@ -30,47 +42,73 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LabTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+            val nombre: MutableState<String> = remember { mutableStateOf("") }
+            val lista = remember { mutableStateListOf<String>() }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                TextField(
+                    value = nombre.value,
+                    onValueChange = {nombre.value = it},
+                    placeholder = {Text("Nombre")},
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Button(
+                    onClick = {
+                        lista.add(nombre.value)
+                        nombre.value = ""
+                    }
+                ){
+                    Text("Guardar")
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Listado de nombres y posicion en la lista")
+                    Button(
+                        onClick = {lista.clear()}
+                    ) {
+                        Text("Limpiar")
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .border(2.dp, Color.Red, RoundedCornerShape(8.dp))
+                ) {
+                    itemsIndexed(lista){ index, item ->
+
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(10.dp),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ){
+                            Text (text= item)
+                            Text(text =(index + 1).toString())
+                        }
+                    }
                 }
             }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-
-        ) {
-        val usuario: MutableState<String> = remember {
-            mutableStateOf("")}
-            TextField(
-                value = usuario.value,
-                onValueChange = {
-                    usuario.value = it
-                },
-                placeholder = {Text("Nombre")}
-                )
-
-    }
-    LazyColumn() { }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LabTheme {
-        Greeting("Android")
-    }
-}
